897a3271d85cd5b18d9a58680c4758161f6a17e5
calculadoranutricional <calculadoranutricional@gmail.com>
2026-02-16 19:47:08 -0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,1 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/tuti/', // ⬅️ AÑADE ESTA LÍNEA (nombre de tu repo)
-})
+import { defineConfig } from 'vite'; import react from '@vitejs/plugin-react-swc'; export default defineConfig({ plugins: [react()], base: '/react/' });
